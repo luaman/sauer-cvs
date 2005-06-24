c62433dd8a9bd650e59ed1456a1b9677d46c0f85
eihrul <eihrul>
2005-06-24 16:00:11 +0000

    sprintf fixes

--- src/clientextras.cpp
+++ src/clientextras.cpp
@@ -64,7 +64,7 @@ vector<sline> scorelines;
 void renderscore(dynent *d)
 {
     sprintf_sd(lag)("%d", d->plag);
-    sprintf_s(scorelines.add().s)("%d\t%s\t%d\t%s\t%s", d->frags, d->state==CS_LAGGED ? "LAG" : lag, d->ping, (int)d->team, (int)d->name);
+    sprintf_s(scorelines.add().s)("%d\t%s\t%d\t%s\t%s", d->frags, d->state==CS_LAGGED ? "LAG" : lag, d->ping, d->team, d->name);
     menumanual(0, scorelines.length()-1, scorelines.last().s); 
 };
 
