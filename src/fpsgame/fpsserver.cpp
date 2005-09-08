5055bf19b28d4daf108233e80b8270d9721e514d
eihrul <eihrul>
2005-09-08 17:01:13 +0000

    added fpsserver

---
+++ src/fpsgame/fpsserver.cpp
@@ -0,0 +1,16 @@
+// clientgame.cpp: core game related stuff
+
+#include "pch.h"
+
+#include "cube.h"
+
+#include "igame.h"
+#include "iengine.h"
+
+#include "game.h"
+
+#include "fpsserver.h"
+
+REGISTERSERVER(fpsgame, "fps", fpsserver);
+
+
