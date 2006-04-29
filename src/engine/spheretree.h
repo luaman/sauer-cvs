6a9261f301047036a6ccb25e72b9cecf1fa0e9f2
eihrul <eihrul>
2006-04-29 19:51:56 +0000

    cleaned up intersect code

--- src/engine/spheretree.h
+++ src/engine/spheretree.h
@@ -5,7 +5,6 @@ struct SphereTree
 
     virtual ~SphereTree() {};
 
-#if 0
     virtual bool intersect(const vec &o, const vec &ray, float maxdist, float &dist) const
     {
         vec tocenter(center);
@@ -13,26 +12,9 @@ struct SphereTree
         float v = tocenter.dot(ray), 
               inside = radius*radius - tocenter.squaredlen(),
               d = inside + v*v;
-        if(d < 0) return false;
-        v -= sqrt(d);
-        if(v > maxdist || (v < 0 && inside < 0)) return false;
+        if(inside >= 0) return true;
+        if(d < 0 || v - sqrt(d) > maxdist) return false;
         return true;
-    };
-#endif
-
-    virtual bool intersect(const vec &o, const vec &ray, float maxdist, float &dist) const
-    {
-       vec fromcenter(o);
-       fromcenter.sub(center);
-       float b = ray.dot(fromcenter),
-             c = fromcenter.squaredlen() - radius*radius,
-             d = b*b - c;
-       if(c <= 0) return true;
-       if(d < 0) return false;
-       d = sqrt(d);
-       float f1 = d-b, f2 = -(d+b);
-       if((f1 < 0 || f1 > maxdist) && (f2 < 0 || f2 > maxdist)) return false;
-       return true;
     };
 
     virtual bool isleaf() { return false; };
