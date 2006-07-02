dc4eb7559ebd1fed30bbc1af7fd7bfe9cc2aa4aa
eihrul <eihrul>
2006-07-02 00:55:04 +0000

    *** empty log message ***

--- src/engine/octarender.cpp
+++ src/engine/octarender.cpp
@@ -1463,7 +1463,6 @@ void renderoutline()
 
     glPushMatrix();
 
-    glDepthFunc(GL_LEQUAL);
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glColor3f(0, 0, 0);
 
@@ -1484,8 +1483,7 @@ void renderoutline()
     };
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glDepthFunc(GL_LESS);
-    
+
     glPopMatrix();
 
     if(hasVBO) glBindBuffer_(GL_ARRAY_BUFFER_ARB, 0);
