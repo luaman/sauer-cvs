1dd40072a0d2d62479f991fdfbf0516881a56306
gilt <gilt>
2006-07-02 02:05:20 +0000

    *** empty log message ***

--- src/shared/geom.cpp
+++ src/shared/geom.cpp
@@ -16,24 +16,25 @@ bool raysphereintersect(vec c, float radius, const vec &o, const vec &ray, float
 
 bool rayrectintersect(const ivec &b, const ivec &s, const vec &o, const vec &ray, float &dist, int &orient)
 {
-	float t = dist;
-	loopi(6)
-	{
-		int d = i>>1, dc = i&1; orient = i;
-		if(dc == ray[d]>0) continue;
-		float dist = t;
-		plane pl(d, b[D[d]]+s[D[d]]*dc);
-		if(pl.rayintersect(o, ray, dist))
-		{
-			vec v(ray);
-		    v.mul(dist);
-		    v.add(o);
-			if(v[R[d]] >= b[R[d]] && v[R[d]] <= b[R[d]]+s[R[d]]
-			&& v[C[d]] >= b[C[d]] && v[C[d]] <= b[C[d]]+s[C[d]])
-				return true;
-		};
-	};
-	dist = t;
-	return false;
+    loopi(6)
+    {
+        int d = i>>1, dc = i&1; orient = i;
+        if(dc == ray[d]>0) continue;
+        float t = 0;
+        plane pl(d, b[D[d]]+s[D[d]]*dc);
+        if(pl.rayintersect(o, ray, t))
+        {
+            vec v(ray);
+            v.mul(t);
+            v.add(o);
+            if(v[R[d]] >= b[R[d]] && v[R[d]] <= b[R[d]]+s[R[d]]
+            && v[C[d]] >= b[C[d]] && v[C[d]] <= b[C[d]]+s[C[d]])
+            {
+                dist += t;
+                return true;
+            };
+        };
+    };
+    return false;
 };
 
