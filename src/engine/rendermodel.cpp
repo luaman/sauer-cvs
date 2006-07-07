5b512fd93a114d9461b6a3d9dd8e1c3547096ab9
eihrul <eihrul>
2006-07-07 01:23:45 +0000

    *** empty log message ***

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -101,9 +101,6 @@ COMMAND(mapmodelreset, "");
 
 hashtable<const char *, model *> mdllookup;
 
-model *modelloaded(const char *name, int i)
-{
-        
 model *loadmodel(const char *name, int i)
 {
     if(!name)
