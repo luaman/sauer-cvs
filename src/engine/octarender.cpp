ab14ccbe8f40f0d8b0ae3dea13262a4c5a41e56d
eihrul <eihrul>
2007-01-20 01:21:47 +0000

    *** empty log message ***

--- src/engine/octarender.cpp
+++ src/engine/octarender.cpp
@@ -1898,7 +1898,6 @@ void setupTMUs()
         glActiveTexture_(GL_TEXTURE0_ARB);
         glProgramEnvParameter4f_(GL_FRAGMENT_PROGRAM_ARB, 5, hdr.ambient/255.0f, hdr.ambient/255.0f, hdr.ambient/255.0f, 0);
         glProgramEnvParameter4f_(GL_VERTEX_PROGRAM_ARB, 6, lastmillis/1000.0f, lastmillis/1000.0f, lastmillis/1000.0f, 0);
-        glProgramEnvParameter4f_(GL_FRAGMENT_PROGRAM_ARB, 6, lastmillis/1000.0f, lastmillis/1000.0f, lastmillis/1000.0f, 0);
     };
 
     glColor4f(1, 1, 1, 1);
