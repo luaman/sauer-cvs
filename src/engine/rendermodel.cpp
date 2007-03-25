e3de74d0cb80f2faa4bd49d844dc875c456dd6d0
eihrul <eihrul>
2007-03-25 08:14:26 +0000

    *** empty log message ***

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -337,13 +337,10 @@ void rendermodel(vec &color, vec &dir, const char *mdl, int anim, int varseed, i
         glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
         vec shaddir = dir;
-        if(shaddir.z<1)
-        {
-            shaddir.z = 0;
-            shaddir.normalize();
-            shaddir.z = 1.5f*(dir.z*0.5f+1);
-            shaddir.normalize();
-        };
+        shaddir.z = 0;
+        if(!shaddir.iszero()) shaddir.normalize();
+        shaddir.z = 1.5f*(dir.z*0.5f+1);
+        shaddir.normalize();
 
         glColor4f(0, 0, 0, dynshadow/100.0f);
         glPushMatrix();
