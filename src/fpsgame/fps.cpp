b3fb35423a7454ce02de13f4eb243c810e31a1c6
eihrul <eihrul>
2007-04-15 19:21:04 +0000

    *** empty log message ***

--- src/fpsgame/fps.cpp
+++ src/fpsgame/fps.cpp
@@ -225,7 +225,7 @@ struct fpsclient : igameclient
         {
             moveplayer(player1, 20, true);
             if(player1->physstate>=PHYS_SLOPE) swaymillis += curtime;
-            float k = pow(0.5f, curtime/10.0f);
+            float k = pow(0.7f, curtime/10.0f);
             swaydir.mul(k); 
             swaydir.add(vec(player1->vel).mul((1-k)/(20*player1->maxspeed)));
             et.checkitems();
