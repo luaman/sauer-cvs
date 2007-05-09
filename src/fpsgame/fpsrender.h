32ede84b889c3c718ef91548b0b074e4e8efe71f
eihrul <eihrul>
2007-05-09 21:59:26 +0000

    *** empty log message ***

--- src/fpsgame/fpsrender.h
+++ src/fpsgame/fpsrender.h
@@ -4,7 +4,7 @@ struct fpsrender
     void renderplayer(fpsent *d, const char *mdlname)
     {
 //      static const char *vweps[] = {NULL, "vwep/shotg", "vwep/chaing", "vwep/rocket", "vwep/rifle", "vwep/gl", "vwep/pistol"};
-        static const char *vweps[] = {NULL, "monster/ogro/vwep", "monster/ogro/vwep", "monster/ogro/vwep", "monster/ogro/vwep", "monster/ogro/vwep", "monster/ogro/vwep"};
+        static const char *vweps[] = {NULL, "vwep/chaing", "vwep/chaing", "vwep/chaing", "vwep/chaing", "vwep/chaing", "vwep/chaing"};
         const char *vwepname = d->gunselect<=GUN_PISTOL ? vweps[d->gunselect] : NULL;
         int attack = d->gunselect==GUN_FIST ? ANIM_PUNCH : ANIM_SHOOT;
         renderclient(d, mdlname, vwepname, attack, d->lastaction, d->lastpain);
@@ -17,13 +17,13 @@ struct fpsrender
         fpsent *d;
         loopv(cl.players) if((d = cl.players[i]) && d->state!=CS_SPECTATOR)
         {
-            const char *mdlname = m_teammode ? (isteam(cl.player1->team, d->team) ? "monster/ogro/blue" : "monster/ogro/red") : (gamemode!=1 ? "monster/ogro/red" : "monster/ogro");
+            const char *mdlname = m_teammode ? (isteam(cl.player1->team, d->team) ? "ironsnout/blue" : "ironsnout/red") : "ironsnout";
             if(d->state!=CS_DEAD || d->superdamage<50) renderplayer(d, mdlname);
             s_strcpy(d->info, d->name);
             if(d->maxhealth>100) { s_sprintfd(sn)(" +%d", d->maxhealth-100); s_strcat(d->info, sn); }
-            if(d->state!=CS_DEAD) particle_text(d->abovehead(), d->info, m_mp(gamemode) && gamemode!=1 ? (isteam(cl.player1->team, d->team) ? 16 : 13) : 11, 1);
+            if(d->state!=CS_DEAD) particle_text(d->abovehead(), d->info, m_teammode ? (isteam(cl.player1->team, d->team) ? 16 : 13) : 11, 1);
         }
-        if(isthirdperson()) renderplayer(cl.player1, m_mp(gamemode) && gamemode!=1 ? "monster/ogro/blue" : "monster/ogro");
+        if(isthirdperson()) renderplayer(cl.player1, m_teammode ? "ironsnout/blue" : "ironsnout");
 
         cl.ms.monsterrender();
         cl.et.renderentities();
