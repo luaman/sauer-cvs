d53d639fc7780375af290fa4ae9567f135637c93
eihrul <eihrul>
2007-07-17 06:20:23 +0000

    *** empty log message ***

--- src/fpsgame/weapon.h
+++ src/fpsgame/weapon.h
@@ -277,7 +277,7 @@ struct weaponstate
         particle_splash(0, 200, 300, v);
         playsound(S_RLHIT, &v);
         particle_fireball(v, RL_DAMRAD, gun==GUN_RL ? 22 : 23);
-        adddynlight(v, 1.5f*RL_DAMRAD, vec(1, 0.75f, 0.5f), 950, 50);
+        adddynlight(v, 1.25f*RL_DAMRAD, vec(1, 0.75f, 0.5f), 900, 100);
         int numdebris = rnd(maxdebris()-5)+5;
         vec debrisvel = vec(owner->o).sub(v).normalize(), debrisorigin(v);
         if(gun==GUN_RL) debrisorigin.add(vec(debrisvel).mul(8));
