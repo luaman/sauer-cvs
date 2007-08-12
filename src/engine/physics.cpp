6438d481d1efc1d3501d695bb6dc7e00c1c9c03f
eihrul <eihrul>
2007-08-12 15:27:40 +0000

    *** empty log message ***

--- src/engine/physics.cpp
+++ src/engine/physics.cpp
@@ -1058,6 +1058,7 @@ void vectoyawpitch(const vec &v, float &yaw, float &pitch)
 }
 
 VARP(maxroll, 0, 3, 20);
+VAR(spectatorspeed, 10, 100, 1000);
 
 void modifyvelocity(physent *pl, bool local, bool water, bool floating, int curtime)
 {
@@ -1136,6 +1137,7 @@ void modifyvelocity(physent *pl, bool local, bool water, bool floating, int curt
     vec d(m);
     d.mul(pl->maxspeed);
     if(!water && !floating) d.mul((pl->move && !pl->strafe ? 1.3f : 1.0f) * (pl->physstate < PHYS_SLOPE && pl->move>=0 ? 1.3f : 1.0f)); // EXPERIMENTAL
+    else if(pl->state==CS_SPECTATOR) d.mul(spectatorspeed/100.0f);
     float friction = water && !floating ? 20.0f : (pl->physstate >= PHYS_SLOPE || floating ? 6.0f : 30.0f);
     float fpsfric = friction/curtime*20.0f;
 
