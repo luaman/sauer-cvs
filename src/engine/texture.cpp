83c8e13976d001515864cc8e55e5236fc6880043
eihrul <eihrul>
2007-08-25 15:15:39 +0000

    *** empty log message ***

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -934,6 +934,7 @@ GLuint genenvmap(const vec &o, int envmapsize)
     else glGenerateMipmap_(GL_TEXTURE_CUBE_MAP_ARB);
     if(rendertex) glDeleteTextures(1, &rendertex);
     glViewport(0, 0, screen->w, screen->h);
+    clientkeepalive();
     return tex;
 }
 
