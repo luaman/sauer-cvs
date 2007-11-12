11ed5c139b1ca1d1b212432ef644d2f746f8aa33
eihrul <eihrul>
2007-11-12 19:14:35 +0000

    *** empty log message ***

--- src/fpsgame/scoreboard.h
+++ src/fpsgame/scoreboard.h
@@ -80,7 +80,7 @@ struct scoreboard : g3d_callback
         loopi(cl.numdynents())
         {
             fpsent *o = (fpsent *)cl.iterdynents(i);
-            if(o && o->type!=ENT_AI)
+            if(o && o->type==ENT_PLAYER)
             {
                 teamscore *ts = NULL;
                 loopv(teamscores) if(!strcmp(teamscores[i].team, o->team)) { ts = &teamscores[i]; break; }
@@ -127,7 +127,7 @@ struct scoreboard : g3d_callback
         loopi(cl.numdynents())
         {
             fpsent *o = (fpsent *)cl.iterdynents(i);
-            if(!o || o->type==ENT_AI || (!showconnecting() && !o->name[0])) continue;
+            if(!o || o->type!=ENT_PLAYER || (!showconnecting() && !o->name[0])) continue;
             if(o->state==CS_SPECTATOR) { spectators.add(o); continue; }
             const char *team = m_teammode && o->team[0] ? o->team : NULL;
             bool found = false;
