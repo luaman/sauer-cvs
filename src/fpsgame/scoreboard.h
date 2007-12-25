ed4c89fdf2225f18834f98be03238dc208dbd911
eihrul <eihrul>
2007-12-25 06:08:21 +0000

    *** empty log message ***

--- src/fpsgame/scoreboard.h
+++ src/fpsgame/scoreboard.h
@@ -64,7 +64,7 @@ struct scoreboard : g3d_callback
         loopi(cl.numdynents())
         {
             fpsent *o = (fpsent *)cl.iterdynents(i);
-            if(o && o->type!=ENT_AI && o->state!=CS_SPECTATOR) best.add(o);
+            if(o && o->type==ENT_PLAYER) best.add(o);
         }
         best.sort(playersort);   
         while(best.length()>1 && best.last()->frags < best[0]->frags) best.drop();
