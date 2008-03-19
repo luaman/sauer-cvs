8b69d760dc01d3e98a00c99f6d464d2d4bcb78d3
eihrul <eihrul>
2008-03-19 22:25:48 +0000

    isthirdperson cleanup

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -557,7 +557,7 @@ VAR(thirdperson, 0, 0, 1);
 VAR(thirdpersondistance, 10, 50, 1000);
 physent *camera1 = NULL;
 bool deathcam = false;
-bool isthirdperson() { return player!=camera1 || player->state==CS_DEAD || (reflecting && !refracting); }
+bool isthirdperson() { return player!=camera1 || player->state==CS_DEAD || reflecting; }
 
 void recomputecamera()
 {
