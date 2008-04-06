b77c79da03d86a7bbdf975629dc325161a7a40bb
eihrul <eihrul>
2008-04-06 17:41:05 +0000

    flag effects

--- src/fpsgame/ctf.h
+++ src/fpsgame/ctf.h
@@ -3,7 +3,7 @@
  
 struct ctfstate
 {
-    static const int FLAGRADIUS = 12;
+    static const int FLAGRADIUS = 16;
 
     struct flag
     {
@@ -15,6 +15,9 @@ struct ctfstate
         bool pickup;
         fpsent *owner;
         extentity *ent;
+        vec interploc;
+        float interpangle;
+        int interptime;
 #endif
 
         flag() { reset(); }
@@ -27,6 +30,9 @@ struct ctfstate
 #else
             pickup = false;
             owner = NULL;
+            interploc = vec(0, 0, 0);
+            interpangle = 0;
+            interptime = 0;
 #endif
             score = 0;
             droptime = 0;
@@ -288,6 +294,22 @@ struct ctfclient : ctfstate
         glDisable(GL_BLEND);
     }
 
+    vec interpflagpos(flag &f, float &angle)
+    {
+        vec pos = f.owner ? f.owner->abovehead() : (f.droptime ? f.droploc : f.spawnloc);
+        angle = f.owner ? f.owner->yaw : (f.ent ? f.ent->attr1 : 0);
+        if(pos.x < 0) return pos;
+        if(f.interptime && f.interploc.x >= 0) 
+        {
+            float t = min((cl.lastmillis - f.interptime)/500.0f, 1.0f);
+            pos.lerp(f.interploc, pos, t);
+            angle += (1-t)*(f.interpangle - angle);
+        }
+        return pos;
+    }
+
+    vec interpflagpos(flag &f) { float angle; return interpflagpos(f, angle); }
+
     void renderflags()
     {
         loopi(2)
@@ -295,9 +317,10 @@ struct ctfclient : ctfstate
             flag &f = flags[i];
             if(!f.ent || (!f.owner && f.droptime && f.droploc.x < 0)) continue;
             const char *flagname = ctfteamflag(cl.player1->team)==i ? "flags/blue" : "flags/red";
+            float angle;
+            vec pos = interpflagpos(f, angle);
             rendermodel(!f.droptime && !f.owner ? &f.ent->light : NULL, flagname, ANIM_MAPMODEL|ANIM_LOOP,
-                        f.owner ? f.owner->abovehead() : (f.droptime ? f.droploc : f.spawnloc), 
-                        f.owner ? f.owner->yaw : f.ent->attr1, 0,  
+                        interpflagpos(f), angle, 0,  
                         MDL_SHADOW | MDL_CULL_VFC | MDL_CULL_OCCLUDED | (f.droptime || f.owner ? MDL_LIGHT : 0));
         }
     }
@@ -347,21 +370,59 @@ struct ctfclient : ctfstate
                 f.owner = owner==cl.player1->clientnum ? cl.player1 : cl.newclient(owner);
                 f.droptime = dropped;
                 f.droploc = droploc;
+                f.interptime = 0;
             }
         }
     }
 
     void dropflag(fpsent *d, int i, const vec &droploc)
     {
-        ctfstate::dropflag(i, droploc, 1);
         flag &f = flags[i];
-        if(!droptofloor(f.droploc, 2, 0)) f.droploc = vec(-1, -1, -1);
+        f.interploc = interpflagpos(f, f.interpangle);
+        f.interptime = cl.lastmillis;
+        ctfstate::dropflag(i, droploc, 1);
+        if(!droptofloor(f.droploc, 2, 0)) 
+        {
+            f.droploc = vec(-1, -1, -1);
+            f.interptime = 0;
+        }
         conoutf("%s dropped %s flag", d==cl.player1 ? "you" : cl.colorname(d), i==ctfteamflag(cl.player1->team) ? "your" : "the enemy");
         playsound(S_FLAGDROP);
     }
 
+    void flagexplosion(int i, const vec &loc)
+    {
+        int ftype;
+        vec color;
+        if(i==ctfteamflag(cl.player1->team)) { ftype = 36; color = vec(0.25f, 0.25f, 1); }
+        else { ftype = 35; color = vec(1, 0.25f, 0.25f); }
+        particle_fireball(loc, 30, ftype);
+        adddynlight(loc, 35, color, 900, 100);
+        particle_splash(0, 150, 300, loc);
+    }
+
+    void flageffect(int i, const vec &from, const vec &to)
+    {
+        vec fromexp(from), toexp(to);
+        if(from.x >= 0) 
+        {
+            fromexp.z += 8;
+            flagexplosion(i, fromexp);
+        }
+        if(to.x >= 0) 
+        {
+            toexp.z += 8;
+            flagexplosion(i, toexp);
+        }
+        if(from.x >= 0 && to.x >= 0)
+            particle_flare(fromexp, toexp, 600, i==ctfteamflag(cl.player1->team) ? 30 : 29);
+    }
+ 
     void returnflag(fpsent *d, int i)
     {
+        flag &f = flags[i];
+        flageffect(i, interpflagpos(f), f.spawnloc);
+        f.interptime = 0;
         ctfstate::returnflag(i);
         conoutf("%s returned %s flag", d==cl.player1 ? "you" : cl.colorname(d), i==ctfteamflag(cl.player1->team) ? "your" : "the enemy");
         playsound(S_FLAGRETURN);
@@ -369,6 +430,9 @@ struct ctfclient : ctfstate
 
     void resetflag(int i)
     {
+        flag &f = flags[i];
+        flageffect(i, interpflagpos(f), f.spawnloc);
+        f.interptime = 0;
         ctfstate::returnflag(i);
         conoutf("%s flag reset", i==ctfteamflag(cl.player1->team) ? "your" : "the enemy");
         playsound(S_FLAGRESET);
@@ -376,9 +440,11 @@ struct ctfclient : ctfstate
 
     void scoreflag(fpsent *d, int relay, int goal, int score)
     {
-        ctfstate::returnflag(relay);
         flag &f = flags[goal];
+        flageffect(goal, flags[goal].spawnloc, flags[relay].spawnloc);
         f.score = score;
+        f.interptime = 0;
+        ctfstate::returnflag(relay);
         if(d!=cl.player1)
         {
             s_sprintfd(ds)("@%d", score);
@@ -391,6 +457,8 @@ struct ctfclient : ctfstate
     void takeflag(fpsent *d, int i)
     {
         flag &f = flags[i];
+        f.interploc = interpflagpos(f);
+        f.interptime = cl.lastmillis;
         conoutf("%s %s %s flag", d==cl.player1 ? "you" : cl.colorname(d), f.droptime ? "picked up" : "stole", i==ctfteamflag(cl.player1->team) ? "your" : "the enemy");
         ctfstate::takeflag(i, d);
         playsound(S_FLAGPICKUP);
@@ -404,7 +472,7 @@ struct ctfclient : ctfstate
         {
             flag &f = flags[i];
             if(!f.ent || f.owner || (f.droptime ? f.droploc.x<0 : ctfteamflag(d->team)==i)) continue;
-            if(o.dist(f.droptime ? f.droploc : f.spawnloc) < 12)
+            if(o.dist(f.droptime ? f.droploc : f.spawnloc) < FLAGRADIUS)
             {
                 if(f.pickup) continue;
                 cl.cc.addmsg(SV_TAKEFLAG, "ri", i);
