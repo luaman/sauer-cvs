42de9d5d36928cbf36a2cf955f47935dd35074b3
eihrul <eihrul>
2008-04-08 02:27:03 +0000

    matrix3x3 cleanup

--- src/engine/lightmap.cpp
+++ src/engine/lightmap.cpp
@@ -320,11 +320,11 @@ void generate_lumel(const float tolerance, const vector<const extentity *> &ligh
             // transform to tangent space
             extern float orientation_tangent[3][4];
             extern float orientation_binormal[3][4];            
-            matrix3x3 m(vec((float *)&orientation_tangent[dimension(lmorient)]), vec((float *)&orientation_binormal[dimension(lmorient)]), normal);      
-            m.orthonormalize();
+            vec S(orientation_tangent[dimension(lmorient)]), 
+                T(orientation_binormal[dimension(lmorient)]);
+            normal.orthonormalize(S, T);
             avgray.normalize();
-            m.transform(avgray); 
-            lm_ray[y*lm_w+x].add(avgray);
+            lm_ray[y*lm_w+x].add(vec(S.dot(avgray), T.dot(avgray), normal.dot(avgray)));
             break;
     }
     sample.x = min(255.0f, max(r, float(ambient)));
