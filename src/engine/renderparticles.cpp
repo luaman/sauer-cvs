a6a136f78ab2c77465f7736e1268307bcb1e2fdf
eihrul <eihrul>
2008-04-10 16:10:28 +0000

    bias tweaks

--- src/engine/renderparticles.cpp
+++ src/engine/renderparticles.cpp
@@ -8,8 +8,8 @@
 VARP(depthfxfpscale, 1, 1<<12, 1<<16);
 VARP(depthfxscale, 1, 1<<6, 1<<8);
 VARP(depthfxblend, 1, 16, 64);
-VAR(depthfxmargin, 0, 1, 64);
-VAR(depthfxbias, 0, 4, 64);
+VAR(depthfxmargin, 0, 16, 64);
+VAR(depthfxbias, 0, 1, 64);
 
 extern void cleanupdepthfx();
 VARFP(fpdepthfx, 0, 1, 1, cleanupdepthfx());
@@ -969,7 +969,7 @@ struct fireballrenderer : listrenderer
             dir.sub(p->o);
             float dist = dir.magnitude();
             dir.mul(psize/dist).add(p->o);
-            float depth = max(-(dir.x*mm[2] + dir.y*mm[6] + dir.z*mm[10] + mm[14]) - depthfxmargin, 0.0f);
+            float depth = max(-(dir.x*mm[2] + dir.y*mm[6] + dir.z*mm[10] + mm[14]) - depthfxbias, 0.0f);
 
             loopk(3)
             {
