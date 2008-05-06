2793b4d042ec98fe721876808fb9c2a096b717e5
eihrul <eihrul>
2008-05-06 17:42:52 +0000

    _MAXDEFSTR -> MAXSTRLEN

--- src/xcode/macutils.mm
+++ src/xcode/macutils.mm
@@ -1,8 +1,8 @@
 #import <Cocoa/Cocoa.h>
 
-#define _MAXDEFSTR 260
+#define MAXSTRLEN 260
 inline char *s_strncpy(char *d, const char *s, size_t m) { strncpy(d,s,m); d[m-1] = 0; return d; };
-inline char *s_strcat(char *d, const char *s) { size_t n = strlen(d); return s_strncpy(d+n,s,_MAXDEFSTR-n); };
+inline char *s_strcat(char *d, const char *s) { size_t n = strlen(d); return s_strncpy(d+n,s,MAXSTRLEN-n); };
 
 void mac_pasteconsole(char *commandbuf)
 {	
