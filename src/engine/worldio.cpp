a69eb98c6a6d91b662af6c76f20dc6356187aa84
eihrul <eihrul>
2008-06-16 23:49:58 +0000

    independent clip and volume materials

--- src/engine/worldio.cpp
+++ src/engine/worldio.cpp
@@ -194,7 +194,16 @@ void loadc(gzFile f, cube &c)
     else
     {
         uchar mask = gzgetc(f);
-        if(mask & 0x80) ext(c).material = gzgetc(f);
+        if(mask & 0x80) 
+        {
+            int mat = gzgetc(f);
+            if(hdr.version < 27)
+            {
+                static uchar matconv[] = { MAT_AIR, MAT_WATER, MAT_CLIP, MAT_GLASS|MAT_CLIP, MAT_NOCLIP, MAT_LAVA|MAT_DEATH, MAT_AICLIP, MAT_DEATH };
+                mat = size_t(mat) < sizeof(matconv)/sizeof(matconv[0]) ? matconv[mat] : MAT_AIR;
+            }
+            ext(c).material = mat;
+        }
         if(mask & 0x3F)
         {
             uchar lit = 0, bright = 0;
