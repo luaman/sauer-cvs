c0124fea39047d226ee63928a8f782ea27598e11
eihrul <eihrul>
2008-09-12 03:45:15 +0000

    hashtable memory cleanup

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -440,6 +440,7 @@ template <class K, class T> struct hashtable
     ~hashtable()
     {
         DELETEA(table);
+        deletechunks();
     }
 
     chain *insert(const K &key, uint h)
@@ -507,17 +508,22 @@ template <class K, class T> struct hashtable
         return false;
     }
 
+    void deletechunks()
+    {
+        for(chainchunk *nextchunk; chunks; chunks = nextchunk)
+        {
+            nextchunk = chunks->next;
+            delete chunks;
+        }
+    }
+
     void clear()
     {
         if(!numelems) return;
         loopi(size) table[i] = NULL;
         numelems = 0;
         unused = NULL;
-        for(chainchunk *nextchunk; chunks; chunks = nextchunk)
-        {
-            nextchunk = chunks->next;
-            delete chunks;
-        }
+        deletechunks();
     }
 };
 
